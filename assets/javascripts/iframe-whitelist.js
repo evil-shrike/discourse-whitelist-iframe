5c99ae1fd3c41574fb630d6fbd3e511e454cd268
Sergei Dorogin <github@dorogin.com>
2017-02-25 03:09:27 +0300

    added Facebook

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -4,4 +4,5 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.youtube\.com\/embed\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.facebook\.com\/plugins\/.+/i);  
 })();
